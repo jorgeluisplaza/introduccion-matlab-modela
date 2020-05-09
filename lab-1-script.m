c424e73e2f0ea259e9fbc1d13de8acf9a359537d
Jorge <jorge.plaza@usach.cl>
2020-05-09 17:04:53 -0400

    Subiendo lab

---
+++ lab-1-script.m
@@ -0,0 +1,10 @@
+% Graficar por separado y en conjunto, las funciones
+% a(x) = 8log5(4x + 12) (2)
+% b(x) = sen(6(log2(x + 9))) + cos(7(log6(4x + 32)))
+
+% La primera funcion debe ser graficada en rojo con *, 
+% la segunda con + y en verde, en el
+% intervalo [0, 15π] con un espacio entre ellos de 0.01.
+
+% Guardamos la primera funcion en la variable y
+y = 8 * (log(4 * x + 12) / log(5))
